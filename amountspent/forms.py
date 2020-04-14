7f881c5ea73f17277fd04daaf3e403250566f4a9
Xzib <syedzohaibali94@gmail.com>
2020-04-15 00:20:42 +0500

    added mobile support in form

--- amountspent/forms.py
+++ amountspent/forms.py
@@ -5,5 +5,5 @@ class DateInput(forms.DateInput):
     input_type = 'date'
 
 class AmountDate(forms.Form):
-    start_date = forms.DateField(label = 'Start Date' , widget = DateInput(attrs={'class':'mx-1 px-4'}))
-    end_date = forms.DateField(label = 'End Date', widget = DateInput(attrs={'class':'mx-1 px-4'}))
+    start_date = forms.DateField(label = 'Start Date' , widget = DateInput(attrs={'class':'form-control form-control-lg mx-1 px-4'}))
+    end_date = forms.DateField(label = 'End Date', widget = DateInput(attrs={'class':'form-control form-control-lg mx-1 px-4'}))
